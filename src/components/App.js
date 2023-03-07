7f1476f35af8ca763a423509892858cadf526c4c
breno-aredes <breno_mg10@hotmail.com>
2023-03-07 09:41:11 -0300

    feat: organization of component folders and pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hashtags from "../pages/hashtag/hashtags";
+import Home from "../pages/home/home";
+import Signin from "../pages/signin/signin";
+import Signup from "../pages/signup.js/signup";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/cadastro" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/hashtags" element={<Hashtags />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
