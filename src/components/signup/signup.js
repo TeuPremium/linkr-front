95b329d01878d4c7537c77d45ab25a14cd4ea459
breno-aredes <breno_mg10@hotmail.com>
2023-03-07 14:06:13 -0300

    feat: functional singup component
    for that:
    1 - install dotenv lib
    2 - imported url from .env file
    3 - post the data to the API
    4 - if succes redirect to signin component

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,16 +1,34 @@
 import { useState } from "react";
 import { BodySignup, Button, StyledLink } from "./styled";
+import axios from "axios";
 
 export default function Signup({ handleShowSignin }) {
-  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [pictureUrl, setPictureUrl] = useState("");
 
-  //onSubmit={signup}
+  async function creatAccount(e) {
+    e.preventDefault();
+
+    const url = `${process.env.REACT_APP_API_URL}/signup`;
+
+    const data = { email, password, username, pictureUrl };
+
+    try {
+      const promisse = await axios.post(url, data);
+
+      if (promisse.data) {
+        handleShowSignin();
+      }
+    } catch (error) {
+      console.log(error.response.status);
+    }
+  }
+
   return (
     <BodySignup>
-      <form>
+      <form onSubmit={creatAccount}>
         <label htmlFor="email">
           <input
             id="email"
