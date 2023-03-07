cac1fc30b3e381372e07464c036dd65b7c46e532
Vini <vinicius.brandaot@gmail.com>
2023-03-07 15:06:08 -0300

    merge branch

--- src/components/Feeds/feedContainer.jsx
+++ src/components/Feeds/feedContainer.jsx
@@ -6,6 +6,7 @@ export default function(prop){
         <h1>timeline</h1>
         <CreatePost>
             <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1"/>
+            
         </CreatePost>
         </Container>
         
