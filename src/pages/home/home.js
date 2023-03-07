6a55c15bb70489943be286037a9804a21c0f705d
breno-aredes <breno_mg10@hotmail.com>
2023-03-07 10:56:15 -0300

    feat: home page created

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,3 +1,10 @@
+import { BodyHome } from "./styled";
+
 export default function Home() {
-  return <div>home aqui</div>;
+  return (
+    <BodyHome>
+      <h1>Linkr</h1>
+      <h2>save, share and discover the best links on the web</h2>
+    </BodyHome>
+  );
 }
---
+++ src/pages/home/styled.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const BodyHome = styled.div`
+  height: 100vh;
+  background-color: #151515;
+  h1 {
+    padding-left: 15%;
+    padding-top: 15%;
+    font-family: "Passion One", cursive;
+    font-size: 106px;
+    font-weight: 700;
+    line-height: 117px;
+    letter-spacing: 0.05em;
+    text-align: left;
+    color: #ffffff;
+  }
+  h2 {
+    padding-left: 15%;
+    width: 442px;
+    font-family: "Oswald", sans-serif;
+    font-size: 43px;
+    font-weight: 700;
+    line-height: 64px;
+    text-align: left;
+    color: #ffffff;
+  }
+`;
