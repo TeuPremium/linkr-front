a069479def96d2a087f76c0a045a76e999032ecb
patrickdemurtas <patrickmurtass@gmail.com>
2023-03-08 15:26:03 -0300

    trendingBox Static

--- src/components/Feeds/feedContainer.jsx
+++ src/components/Feeds/feedContainer.jsx
@@ -14,46 +14,104 @@ export default function (prop) {
 
   return (
     <>
-      <Container>
-        <h1>timeline</h1>
-        <PostContainer color={"white"}>
-          <div>
-            <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
-          </div>
+      <ContainerFeed>
+        <Container>
+          <h1>timeline</h1>
 
-          <WritePostContainer>
-            <h2>What are you going to share today?</h2>
+          <PostContainer color={"white"}>
+            <div>
+              <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
+            </div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <input
-                placeholder="http://..."
-                {...register("url", { required: true })}
-              />
-              <input
-                placeholder="Awesome article about #javascript"
-                {...register("postCommentary", { required: true })}
-              />
-              {errors.postCommentary && <span>This field is required</span>}
-              <div>
-                <input type="submit" />
-              </div>
-            </form>
-          </WritePostContainer>
-        </PostContainer>
-        <PostContainer color={"#171717"}>
-          <div>
-            <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
-          </div>
-          <UsersPosts>
-            <h3>username</h3>
-            <h4>
-              Muito maneiro esse tutorial de Material UI com React, deem uma
-              olhada!
-            </h4>
-            Placeholder box
-          </UsersPosts>
-        </PostContainer>
-      </Container>
+            <WritePostContainer>
+              <h2>What are you going to share today?</h2>
+
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  placeholder="http://..."
+                  {...register("url", { required: true })}
+                />
+                <input
+                  placeholder="Awesome article about #javascript"
+                  {...register("postCommentary", { required: true })}
+                />
+                {errors.postCommentary && <span>This field is required</span>}
+                <div>
+                  <input type="submit" />
+                </div>
+              </form>
+            </WritePostContainer>
+          </PostContainer>
+          <PostContainer color={"#171717"}>
+            <div>
+              <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
+            </div>
+            <UsersPosts>
+              <h3>username</h3>
+              <h4>
+                Muito maneiro esse tutorial de Material UI com React, deem uma
+                olhada!
+              </h4>
+              Placeholder box
+            </UsersPosts>
+          </PostContainer>
+
+          <PostContainer color={"#171717"}>
+            <div>
+              <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
+            </div>
+            <UsersPosts>
+              <h3>username</h3>
+              <h4>
+                Muito maneiro esse tutorial de Material UI com React, deem uma
+                olhada!
+              </h4>
+              Placeholder box
+            </UsersPosts>
+          </PostContainer>
+
+          <PostContainer color={"#171717"}>
+            <div>
+              <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
+            </div>
+            <UsersPosts>
+              <h3>username</h3>
+              <h4>
+                Muito maneiro esse tutorial de Material UI com React, deem uma
+                olhada!
+              </h4>
+              Placeholder box
+            </UsersPosts>
+          </PostContainer>
+        
+          
+        </Container>
+
+        <TrendingBoxContainer>
+
+
+          <HeaderTrending>
+            <h1>trending</h1>
+          </HeaderTrending>
+
+          <HashtagContainer>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+           <h1># hashtag example</h1>
+          </HashtagContainer>
+
+        </TrendingBoxContainer>
+
+      </ContainerFeed>
+
+
     </>
   );
 }
@@ -113,3 +171,52 @@ const Container = styled.div`
   margin-right: auto;
   margin-top: 150px;
 `;
+
+
+const TrendingBoxContainer = styled.div`
+  height: 406px;
+  width: 301px;
+  background-color: #171717;
+  border-radius: 16px;
+  margin-top: 232px;
+  margin-right: 150px;
+
+`
+
+const HeaderTrending = styled.div`
+  width: 100%;
+  height: 61px;
+  border-radius: 16px 16px 0 0;
+  border-bottom: 2px solid #484848;
+  background-color: #171717;
+  display: flex;
+  align-items: center;
+  h1{
+    font-size: 27px;
+    font-weight: 700;
+    margin-left: 16px;
+  }
+`
+
+const HashtagContainer = styled.div`
+  width: 130px;
+  height: 293px;
+  margin-left: 16px;
+  margin-top: 22px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #171717;
+  h1{
+    font-size: 19px;
+
+  }
+`
+
+
+
+const ContainerFeed = styled.div`
+  display: flex;
+  justify-content: space-between;
+  
+`
