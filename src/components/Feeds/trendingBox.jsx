5667d7d7d5dd09046864c15a709f370f90a264d4
Vini <vinicius.brandaot@gmail.com>
2023-03-08 22:17:31 -0300

    fix refactored trending to another archive

---
+++ src/components/Feeds/trendingBox.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+import { TrendingBoxContainer, HeaderTrending, HashtagContainer } from "./styles" 
+
+export default function TrendingTags(prop){
+    return(
+        <>
+         <TrendingBoxContainer>
+
+
+            <HeaderTrending>
+            <h1>trending</h1>
+            </HeaderTrending>
+
+            <HashtagContainer>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            <h1># hashtag example</h1>
+            </HashtagContainer>
+
+        </TrendingBoxContainer>
+        </>
+    )
+}
+
