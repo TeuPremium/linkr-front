69bb4530a0e2c13e444cfabc5158225e052f8e2c
breno-aredes <breno_mg10@hotmail.com>
2023-03-08 21:05:02 -0300

    fix: logout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,9 +6,11 @@ import LinkrLogo from "./linkrLogo";
 import { ContainerHeader, UserContainer } from "./styled";
 
 export default function () {
-  const [showLogout, setShowLogout] = useState(true);
+  const [showLogout, setShowLogout] = useState(false);
+  const [direction, setDirection] = useState("down");
 
-  const handleLogout = () => {
+  const handleClick = () => {
+    setDirection(direction === "down" ? "up" : "down");
     setShowLogout(!showLogout);
   };
 
@@ -17,8 +19,8 @@ export default function () {
       <ContainerHeader>
         <LinkrLogo />
         <UserSearchInput />
-        <UserContainer>
-          <ArrowDown handleLogout={handleLogout} />
+        <UserContainer onClick={handleClick}>
+          <ArrowDown direction={direction} />
           <img src="https://imgs.search.brave.com/7KHxJqOc757ysQ-_b0Tcyzs67rTqJ4Bv5dKrE9P77Us/rs:fit:900:900:1/g:ce/aHR0cHM6Ly92aWdu/ZXR0ZS53aWtpYS5u/b2Nvb2tpZS5uZXQv/eW91dHViZS9pbWFn/ZXMvYi9iMy9HcmFu/ZGF5eS5qcGcvcmV2/aXNpb24vbGF0ZXN0/P2NiPTIwMTgwODAz/MDAyODU1" />
         </UserContainer>
         {showLogout && <Logout />}
