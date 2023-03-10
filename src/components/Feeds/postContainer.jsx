aa2f3061a29eceb55d0c1fdba0d24efaa817f8fa
Vini <vinicius.brandaot@gmail.com>
2023-03-09 22:36:26 -0300

    edit post locally logic

--- src/components/Feeds/postContainer.jsx
+++ src/components/Feeds/postContainer.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import PencilIcon from "../../assets/styles/pencilIcon";
 import TrashIcon from "../../assets/styles/trashIcon";
 import { PostContainer, LikeContainer, HeartIcon, UsersPosts, UserHeader, LinkContainer, CommentContainer } from "./styles";
-
+import { useForm } from "react-hook-form";
 
 export function UserPostContainer(prop){
-  console.log(prop.image)
+    const [edit, setEdit] = useState(false)
+    const [comment, setComment] = useState(prop.comment) 
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+      setEdit(false)
+      setComment(data.comment);
+
+    };
+
+
+    function editChange(boolean){
+      boolean ? setEdit(false) : setEdit(true) 
+    }
+
+    
+   
     return(
     <PostContainer color={"#171717"}>
 
@@ -21,15 +38,17 @@ export function UserPostContainer(prop){
         <UserHeader>
           <h3>{prop.username}</h3>
           <div>
-          <TrashIcon/>
-          <PencilIcon/>
+            <div onClick={() => console.log(true)}>
+            <TrashIcon />
+            </div>
+            <div onClick={() => editChange(edit)}>
+            <PencilIcon/>
+            </div>
           </div>
         </UserHeader>
 
         <CommentContainer>
-          <h4>
-            {prop.comment}
-          </h4>
+        {edit ? <form onSubmit={handleSubmit(onSubmit)}><input defaultValue={comment} {...register("comment")}/></form>: <h4>{comment}</h4>}
         </CommentContainer>
 
         <LinkContainer>
@@ -42,3 +61,4 @@ export function UserPostContainer(prop){
     </PostContainer>
   )
 }
+
